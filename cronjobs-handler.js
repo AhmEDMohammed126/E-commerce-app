109025efb8eafe5a1f1b0f12bc6723d1436c5623
ahmed mohamed <ahmudmohamed78s@gmail.com>
2024-10-06 14:11:37 +0300

    spring-3

--- cronjobs-handler.js
+++ cronjobs-handler.js
@@ -2,7 +2,7 @@ import { cronJobOne, productRatingCron } from "./src/Utils/crons.uitls.js";
 import { gracefulShutdown } from "node-schedule";
 
 export const cronHandler=()=>{
-     //cron jobs
+    //cron jobs
     cronJobOne();
     productRatingCron();
     //gracefulShutdown();
